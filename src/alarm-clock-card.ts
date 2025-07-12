dd9884fd6ff4b544fabc83263d30163aba282428
David Siegel <david@siegel.com>
2025-07-12 12:23:53 -0400

    Version 2.0.11

--- src/alarm-clock-card.ts
+++ src/alarm-clock-card.ts
@@ -1097,7 +1097,7 @@ window.customCards.push({
 });
 
 console.info(
-  `%c  ALARM-CLOCK-CARD  %c  Version 2.0.10  `,
+  `%c  ALARM-CLOCK-CARD  %c  Version 2.0.11  `,
   'color: orange; font-weight: bold; background: black',
   'color: white; font-weight: bold; background: dimgray'
 );
